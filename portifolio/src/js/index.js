80d76f07cde80ecb58fa56e990cb45fe00616c54
Igor Oliveira <127331783+IgorOdSilva@users.noreply.github.com>
2023-07-24 22:18:42 -0300

    Add files via upload

---
+++ portifolio/src/js/index.js
@@ -0,0 +1,26 @@
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelector('.cartao-pokemon')
+
+
+listaSelecaoPokemons.forEach(pokemon => {
+    console.log(pokemon)
+
+    pokemon.addEventListener('click', () => {
+       const cartaoPokemonAberto = document.querySelector('.aberto')
+       cartaoPokemonAberto.classList.remove('aberto') 
+
+       const idPokemonSelecionado = pokemon.attributes.id.value
+
+       const idDoCartaoDoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
+       const cartaoPokemonParaAbrir = document.getElementById('cartao-' + idPokemonSelecionado)
+       cartaoPokemonParaAbrir.classList.add('aberto')
+
+       const pokemonAtivoNaListagem = document.querySelector('.ativo')
+       pokemonAtivoNaListagem.classList.remove('ativo')
+
+       const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+       pokemonSelecionadoNaListagem.classList.add('ativo')
+
+
+    })
+})
